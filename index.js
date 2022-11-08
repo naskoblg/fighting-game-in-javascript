569bb6cce83a4c40ea899d7a82fc17f769b77401
Atanas Tsvetkov <atanas.tsvetkov@refinitiv.com>
2022-11-09 00:29:45 +0200

    End of day one

--- index.js
+++ index.js
@@ -6,20 +6,71 @@ canvas.height = 576;
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
-
+const gravity = 0.2
 class Sprite {
-    constructor(position) {
+    constructor({position, velocity}) {
         this.position = position
+        this.velocity = velocity
+        this.height = 150
     }
 
     draw() {
         c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 50, 150)
+        c.fillRect(this.position.x, this.position.y, 50, this.height)
+    }
+    update() {
+        this.draw()
+        //this.position.y += gravity
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0
+        } else {
+            this.velocity.y += gravity
+        }
     }
 }
 
-const player = new Sprite({x: 0, y: 0})
-player.draw()
+const player = new Sprite({position: {x: 0, y: 0}, velocity: {x: 0, y: 10}})
+//player.draw()
 
+const enemy = new Sprite({position: {x: 400, y: 100}, velocity: {x: 0, y: 10}})
+//enemy.draw()
 
 console.log(player);
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    enemy.update()
+//    console.log('go')
+}
+
+animate()
+
+window.addEventListener('keydown', (event) => {
+    switch(event.key) {
+        case 'd':
+            player.velocity.x = 1
+            break
+        case 'a':
+            player.velocity.x = -1
+            break
+    }
+    console.log(event.key)
+})
+
+window.addEventListener('keyup', (event) => {
+    switch(event.key) {
+        case 'd':
+            player.velocity.x = 0
+            break
+        case 'a':
+        player.velocity.x = -1
+        break
+    }
+    console.log(event.key)
+})
